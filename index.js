e097c5038e0a47875b66f11aae11de12b5eb357e
Sas-Kirakosyan <47293247+Sas-Kirakosyan@users.noreply.github.com>
2019-09-27 22:51:20 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,35 @@ window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     nav.style.top = "0";
+    
   } else {
-    nav.style.top = "-60px";
+    nav.style.top = "-250px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+window.scrollTo( 0, 1000 );
+
+// этот код меняет поведение прокрутки на "smooth"
+window.scrollTo({
+    top: 1000,
+    behavior: "smooth"
+});
+
+
+
+let navMenu = document.querySelector('#navbarsExampleDefault');
+let btnNav = document.querySelector('#btn')
+navMenu.style.display ="none";
+btnNav.addEventListener('click', function(){
+if(navMenu.style.display === "none"){
+  navMenu.style.display ="block";
+}else{
+  navMenu.style.display ="none";
+}
+  
+    // navMenu.setAttribute('id', 'ul');
+
+
+})
+
